0a3df367b1e63bf93ea82ebe3ccbcf19de53b856
stinaStyle <lazorra.lisa@gmail.com>
2016-10-03 19:39:17 +0600

    lab2 minimum

---
+++ Lab_2/Lab_2/Examples.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Examples
+    {
+        static void Example(string[] args)
+        {
+            string[] lines = { "one", "two", "three", "four", "five" };
+            string text = "A class is the most powerful data type in C#. Like a structure, \n" +
+                       "a class defines the data and behavior of the data type. ";
+
+            File.WriteAllLines(@"D:\Лекции C#\методички\Start_learning\Lab_2\lines.txt", lines);
+            File.WriteAllText(@"D:\Лекции C#\методички\Start_learning\Lab_2\text.txt", text);
+            using (StreamWriter file = new StreamWriter(@"D:\Лекции C#\методички\Start_learning\Lab_2\testyourluck.txt"))
+            {
+                file.WriteLine(" \t test your luck");
+                foreach (var line in lines)
+                {
+                    if (!line.Contains("two"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+
+            }
+            // Read the file as one string.
+            string textRead = File.ReadAllText(@"D:\Лекции C#\методички\Start_learning\Lab_2\testyourluck.txt");
+
+            // Display the file contents to the console. Variable text is a string.
+            Console.WriteLine("The text in file testyourluck.txt for {0} is \r\n {1}", DateTime.Now, textRead);
+
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] a = File.ReadAllLines(@"D:\Лекции C#\методички\Start_learning\Lab_2\lines.txt");
+
+
+            // Display the file contents by using a foreach loop.
+            Console.WriteLine("\n Every line in file lines.txt : ");
+            foreach (var line in a)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+
+            string s = File.ReadAllText(@"D:\Лекции C#\методички\Start_learning\Lab_2\text.txt");
+            Console.WriteLine("\n All text in the file text.txt: \t\n {0}", s);
+
+            Console.WriteLine("\n Press any key to exit");
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
--- Lab_2/Lab_2/Program.cs
+++ Lab_2/Lab_2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,51 +10,68 @@ namespace Lab_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string[] lines = {"one", "two", "three", "four", "five"};
-            string text = "A class is the most powerful data type in C#. Like a structure, \n" +
-                       "a class defines the data and behavior of the data type. ";
 
-            File.WriteAllLines(@"D:\Лекции C#\методички\Start_learning\Lab_2\lines.txt", lines);
-            File.WriteAllText(@"D:\Лекции C#\методички\Start_learning\Lab_2\text.txt", text);
-            using (StreamWriter file = new StreamWriter(@"D:\Лекции C#\методички\Start_learning\Lab_2\testyourluck.txt"))
+
+            // Dialog for variables
+            Console.WriteLine("Variable a: ");
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Variable i");
+            var i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Variable c");
+            var c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Variable l");
+            bool l = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Your Name: ");
+            string Name = Console.ReadLine();
+
+            //Write variables to the file LAB_2.txt
+            using (StreamWriter file = new StreamWriter(@"D:\Лекции C#\методички\Start_learning\Lab_2\LAB_2.txt"))
             {
-                file.WriteLine(" \t test your luck");
-                foreach (var line in lines)
+                file.WriteLine("\t a = {0}", a);
+                file.WriteLine("\t i = {0}", i);
+                file.WriteLine("\t c = {0}", c);
+                file.WriteLine("\t l = {0}", l);
+                file.WriteLine("\t Name: {0}", Name);
+            }
+
+            
+
+            using (StreamWriter file1 = new StreamWriter(@"D:\Лекции C#\методички\Start_learning\Lab_2\LAB_2.2.txt"))
+            {
+                
+                //Function 
+                for (int x = -3; x < 4; ++x)
                 {
-                    if (!line.Contains("two"))
-                    {
-                        file.WriteLine(line);
-                    }
+                   var  y = ((1 - (Math.Sqrt(x))) / (1 + (Math.Pow(x, 4))));
+
+
+                    file1.WriteLine(y);
+
+
                 }
+              
 
             }
-            // Read the file as one string.
-            string textRead = File.ReadAllText(@"D:\Лекции C#\методички\Start_learning\Lab_2\testyourluck.txt");
+        }
+
+
+
+
+
+
+
+
+
+
             
-            // Display the file contents to the console. Variable text is a string.
-            Console.WriteLine("The text in file testyourluck.txt for {0} is \r\n {1}", DateTime.Now, textRead);
 
-            // Read each line of the file into a string array. Each element
-            // of the array is one line of the file.
-            string[] a = File.ReadAllLines(@"D:\Лекции C#\методички\Start_learning\Lab_2\lines.txt");
-
-
-            // Display the file contents by using a foreach loop.
-            Console.WriteLine("\n Every line in file lines.txt : ");
-            foreach (var line in a)
-            {
-                // Use a tab to indent each line of the file.
-                Console.WriteLine("\t" + line);
-            }
-
-            string s = File.ReadAllText(@"D:\Лекции C#\методички\Start_learning\Lab_2\text.txt");
-            Console.WriteLine("\n All text in the file text.txt: \t\n {0}", s);
-            
-            Console.WriteLine("\n Press any key to exit");
-            Console.ReadLine();
 
         }
     }
-}
+
