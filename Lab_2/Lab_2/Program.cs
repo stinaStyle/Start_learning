488590083183de54f81a996cbc96b8c4c7d396d3
stinaStyle <lazorra.lisa@gmail.com>
2016-10-03 21:29:42 +0600

    Math.Pow

--- Lab_2/Lab_2/Program.cs
+++ Lab_2/Lab_2/Program.cs
@@ -14,32 +14,6 @@ namespace Lab_2
         {
 
 
-            // Dialog for variables
-            Console.WriteLine("Variable a: ");
-            var a = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Variable i");
-            var i = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Variable c");
-            var c = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Variable l");
-            bool l = Convert.ToBoolean(Console.ReadLine());
-
-            Console.WriteLine("Your Name: ");
-            string Name = Console.ReadLine();
-
-            //Write variables to the file LAB_2.txt
-            using (StreamWriter file = new StreamWriter(@"D:\Лекции C#\методички\Start_learning\Lab_2\LAB_2.txt"))
-            {
-                file.WriteLine("\t a = {0}", a);
-                file.WriteLine("\t i = {0}", i);
-                file.WriteLine("\t c = {0}", c);
-                file.WriteLine("\t l = {0}", l);
-                file.WriteLine("\t Name: {0}", Name);
-            }
-
             
 
             using (StreamWriter file1 = new StreamWriter(@"D:\Лекции C#\методички\Start_learning\Lab_2\LAB_2.2.txt"))
@@ -48,7 +22,7 @@ namespace Lab_2
                 //Function 
                 for (int x = -3; x < 4; ++x)
                 {
-                   var  y = ((1 - (Math.Sqrt(x))) / (1 + (Math.Pow(x, 4))));
+                   var  y = ((1 - (Math.Pow(x, 2))) / (1 + (Math.Pow(x, 4))));
 
 
                     file1.WriteLine(y);
