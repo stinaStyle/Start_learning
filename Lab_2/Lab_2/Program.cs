e3fbf16841b80a57f66f26df78c2bdbe214cf5ca
stinaStyle <lazorra.lisa@gmail.com>
2016-10-05 19:29:29 +0600

    d

--- Lab_2/Lab_2/Program.cs
+++ Lab_2/Lab_2/Program.cs
@@ -10,6 +10,7 @@ namespace Lab_2
 {
     class Program
     {
+        
         static void Main()
         {
             using (StreamWriter file0 = new StreamWriter(@"D:\Лекции C#\методички\Start_learning\Lab_2\LAB_2.1.txt"))
@@ -24,7 +25,7 @@ namespace Lab_2
                 file0.WriteLine(4);
                 file0.WriteLine(3.14);
             }
-       
+            
            
 
 
@@ -32,16 +33,25 @@ namespace Lab_2
             {
                 //Function 
                 file1.WriteLine("1) ((1 - (Math.Pow(x, 2)))/(1 + (Math.Pow(x, 4))))");
-                for (double x = -3; x < 3; x += 0.75)
+                double x0 = -3;
+                double x1 = 3;
+                double d = ((x1 - (x0))/8);
+                for (double x = x0; x < x1; x += d)
                 {
+
                     var y1 = ((1 - (Math.Pow(x, 2)))/(1 + (Math.Pow(x, 4))));
                     file1.WriteLine("1) x = {0} y = {1}", x, y1);
                 }
                 var j = ((1 - (Math.Pow(2, 2)))/(1 + (Math.Pow(2, 4))));
                 file1.WriteLine("1) x = 2  y = {0} ", j);
 
+
                 file1.WriteLine("2) (Math.Sin(x))/((Math.Pow(x, 2)) - 1)");
-                for (double x = -2; x < 2; x += 0.5)
+                x0 = -2;
+                x1 = 2;
+                d = ((x1 - (x0)) / 8);
+                for (double x = x0; x < x1; x += d)
+               
                 {
                     var y2 = (Math.Sin(x))/((Math.Pow(x, 2)) - 1);
                     file1.WriteLine("2) x = {0} y = {1}", x, y2);
@@ -51,7 +61,11 @@ namespace Lab_2
 
 
                 file1.WriteLine("3) (2*Math.PI)/(Math.Pow(x, 2) - Math.PI)");
-                for (double x = -2; x < 4; x += 0.75)
+                x0 = -2;
+                x1 = 4;
+                d = (((x1 - (x0)) / 8));
+                for (double x = x0; x < x1; x += d)
+                
                 {
                     var y3 = (2*Math.PI)/(Math.Pow(x, 2) - Math.PI);
                     file1.WriteLine("3) x = {0} y = {1}", x, y3);
