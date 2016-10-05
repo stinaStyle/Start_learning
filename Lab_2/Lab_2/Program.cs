aa5d6dc63fffb87a9327661f99a13795a52dfe4b
stinaStyle <lazorra.lisa@gmail.com>
2016-10-05 22:16:17 +0600

    methods

--- Lab_2/Lab_2/Program.cs
+++ Lab_2/Lab_2/Program.cs
@@ -10,7 +10,14 @@ namespace Lab_2
 {
     class Program
     {
-        
+        static double CalculateDelta(double x0, double x1)
+        {
+            double d = ((x1 - (x0))/8);
+            return d;
+        }
+
+       
+
         static void Main()
         {
             using (StreamWriter file0 = new StreamWriter(@"D:\Лекции C#\методички\Start_learning\Lab_2\LAB_2.1.txt"))
@@ -32,50 +39,25 @@ namespace Lab_2
             using (StreamWriter file1 = new StreamWriter(@"D:\Лекции C#\методички\Start_learning\Lab_2\LAB_2.2.txt"))
             {
                 //Function 
-                file1.WriteLine("1) ((1 - (Math.Pow(x, 2)))/(1 + (Math.Pow(x, 4))))");
-                double x0 = -3;
-                double x1 = 3;
-                double d = ((x1 - (x0))/8);
-                for (double x = x0; x < x1; x += d)
-                {
+                Calculate(file1, -3, 3, 2, x => ((1 - (Math.Pow(x, 2))) / (1 + (Math.Pow(x, 4)))), "((1 - (Math.Pow(x, 2))) / (1 + (Math.Pow(x, 4))))");
+                Calculate(file1, -2, 2, 1.57, x => (Math.Sin(x)) / ((Math.Pow(x, 2)) - 1), "(Math.Sin(x)) / ((Math.Pow(x, 2)) - 1)");
+                Calculate(file1, -2, 4, 3.14, x => (2 * Math.PI) / (Math.Pow(x, 2) - Math.PI), "(2 * Math.PI) / (Math.Pow(x, 2) - Math.PI)");
 
-                    var y1 = ((1 - (Math.Pow(x, 2)))/(1 + (Math.Pow(x, 4))));
-                    file1.WriteLine("1) x = {0} y = {1}", x, y1);
-                }
-                var j = ((1 - (Math.Pow(2, 2)))/(1 + (Math.Pow(2, 4))));
-                file1.WriteLine("1) x = 2  y = {0} ", j);
-
-
-                file1.WriteLine("2) (Math.Sin(x))/((Math.Pow(x, 2)) - 1)");
-                x0 = -2;
-                x1 = 2;
-                d = ((x1 - (x0)) / 8);
-                for (double x = x0; x < x1; x += d)
-               
-                {
-                    var y2 = (Math.Sin(x))/((Math.Pow(x, 2)) - 1);
-                    file1.WriteLine("2) x = {0} y = {1}", x, y2);
-                }
-                var o = (Math.Sin(1.57))/((Math.Pow(1.57, 2)) - 1);
-                file1.WriteLine("2) x = 1.57 y = {0} ", o);
-
-
-                file1.WriteLine("3) (2*Math.PI)/(Math.Pow(x, 2) - Math.PI)");
-                x0 = -2;
-                x1 = 4;
-                d = (((x1 - (x0)) / 8));
-                for (double x = x0; x < x1; x += d)
-                
-                {
-                    var y3 = (2*Math.PI)/(Math.Pow(x, 2) - Math.PI);
-                    file1.WriteLine("3) x = {0} y = {1}", x, y3);
-                }
-                var p = (2*Math.PI)/(Math.Pow(3.14, 2) - Math.PI);
-                file1.WriteLine("3) x = 3.14 y = {0}", p);
             }
         }
 
+        static void Calculate(StreamWriter files, double x0, double x1, double f, Func<double, double> obj, string s1)
+        {
+            files.WriteLine(s1);          
+            for (double x = x0; x < x1; x += CalculateDelta(x0, x1))
+            {
 
+                var y1 = obj(x);
+                files.WriteLine(" x = {0} y = {1}", x, y1);
+            }
+            var j = obj(f);
+            files.WriteLine(" x = {0}  y = {1} ", f, j);
+        }
 
 
 
