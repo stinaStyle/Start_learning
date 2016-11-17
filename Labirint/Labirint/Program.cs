60dd6d22999e113fcb16db9d30a3065722a1fb85
stinaStyle <lazorra.lisa@gmail.com>
2016-11-17 15:59:44 +0600

    DrawVL

--- Labirint/Labirint/Program.cs
+++ Labirint/Labirint/Program.cs
@@ -12,40 +12,82 @@ namespace Labirint
 
     class Program
     {
+        //объявление основных данных
         public static int x = 15;
         public static int y = 15;
         public static string O = "O";
         public static string[,] box = new string[100, 100];
 
+        //генерация карты
         public static string[,] GenMap(string[,] arr1)
         {
             for (int i = 0; i < 100; i++)
             {
+                string l = "▓";
+                string h = "9";
+                arr1[i, i] = l;
                 for (int j = 0; j < 100; j++)
-                {   string l = "#";
-                    string h = "9";
-                    arr1[i, i] = l;
-                    if (j > 10 && j < 25)
-                    {
-                        arr1[i, i] = h;
-                    }
+                {   
+                    arr1[i, j] = l;
+                    //if (j > 10 && j < 25)
+                    //{
+                    //    arr1[i, i] = h;
+                    //}
                 }
                 
             }
-            //Console.SetCursorPosition(x, y);
-            
-          
             return arr1;
         }
 
+        //отрисовка карты 
         public static void ShowMap(string[,] arr)
         {
             for (int t = 0; t < 100; t++)
             {
-                Console.Write(arr[t, t] + "\n");
-                for (int i = 0; i < 100; i++) { Console.Write(arr[t, t]);}
+                Console.Write("\n");
+                for (int i = 0; i < 19; i++) { Console.Write(" "); }
+                for (int i = 20; i < 100; i++) { Console.Write(arr[t, t]);}
             }
         }
+        //горизонтальные дорожки
+        public static void DrawHL(string[,] arr, int a, int b, int c, int d, int e)
+        {
+            int v = 0;
+            int g = 0;
+            Console.SetCursorPosition(x+3,y);
+            
+            for (int i = 0; i < a; i++)
+            {Console.Write(" a"+i);} //ставлю буковки чтоб видеть где что сработало
+            Console.SetCursorPosition(x+10,y+5);
+            for (int i = 0; i < b; i++)
+            {Console.Write(" b"+i);}
+            Console.SetCursorPosition(x + 30, y + 10);
+            for (int i = 0; i < c; i++)
+            { Console.Write(" c"+i); }
+            Console.SetCursorPosition(x + 50, y + 15);
+            for (int i = 0; i < d; i++)
+            { Console.Write(" d"+i); }
+            Console.SetCursorPosition(x + 70, y + 17);
+            for (int i = 0; i < e; i++)
+            { Console.Write(" e"+i); }
+        }
+
+        //вертикальные дорожки
+        public static void DrawVL(string[,]arr)
+        {
+            Console.SetCursorPosition(x+3,y);
+            for (int i = 0; i < 10; i++)
+            {
+                string g = "g";
+                y = 20;
+                x = i;
+                arr[i, y]= g;
+                Console.Write(arr[i,y]); // todo: сделать чтобы по вертикали вместо заглушки писалась дорожка из "g"
+            }
+            
+        }
+
+        // ползающий символ
         public static void Push()
         {
             
@@ -67,13 +109,27 @@ namespace Labirint
                 Console.Write(O);
             }
         }
+
+        /// ///////////////////////////////////////////////////////////////////////////////////
         
+
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////
+
         static void Main(string[] args)
         {
             GenMap(box);
             ShowMap(box);
+            DrawHL(box, 10, 10, 10,10,10);
+            DrawVL(box);
             Push();
             Console.ReadLine();
         }
+      
+
+
     }
-}
+    }
+
