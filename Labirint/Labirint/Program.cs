2b3ce96f4b10f504523af7948f7767c84bc2ef21
stinaStyle <lazorra.lisa@gmail.com>
2016-11-22 00:19:26 +0600

    Eat();

--- Labirint/Labirint/Program.cs
+++ Labirint/Labirint/Program.cs
@@ -14,12 +14,13 @@ namespace Labirint
     class Program
     {
         //объявление основных данных
-        public static int x = 15;
-        public static int y = 15;
+        public static int x = 0;
+        public static int y = 0;
         public static string O = "O";
-
+        public static int count = 0;
         const string wall = "▓"; // узри же константы
         const string empty = " ";
+        const string point = "$";
 
         public static string[,] box = new string[30, 20]; // размер сделай по-меньше
 
@@ -29,25 +30,6 @@ namespace Labirint
         //public static string[,] GenMap(string[,] arr1) // зачем? У нас есть массив box
         public static void GenMap()
         {
-            // неправильно
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    string l = "▓";
-            //    string h = "9";
-            //    arr1[i, i] = l;
-            //    for (int j = 0; j < 100; j++)
-            //    {
-            //        arr1[i, j] = l;
-            //        //if (j > 10 && j < 25)
-            //        //{
-            //        //    arr1[i, i] = h;
-            //        //}
-            //    }
-
-            //}
-
-            
-
             Random r = new Random();
             for (int i = 0; i < box.GetLength(0); i++)
             {
@@ -56,6 +38,11 @@ namespace Labirint
                     if (r.Next(10) > 5)
                     {
                         box[i, j] = wall;
+                    }
+                    else if (r.Next(10) > 2 && r.Next(10) < 5)
+                    {
+                        box[i, j] = point;
+                        
                     }
                     else
                     {
@@ -68,19 +55,10 @@ namespace Labirint
         //отрисовка карты 
         public static void ShowMap(string[,] arr)
         {
-            /*for (int t = 0; t < 100; t++)
-            {
-                Console.Write("\n");
-                for (int i = 0; i < 19; i++) { Console.Write(" "); }
-                for (int i = 20; i < 100; i++) { Console.Write(arr[t, t]); }
-            }*/ 
-
-            // это было неправильно
-
             // Рисует сгенерированную карту
             var horiz = Math.Max(Console.BufferWidth, arr.GetLength(0));
             var vert = Math.Max(Console.BufferHeight, arr.GetLength(1));
-
+            
             Console.SetBufferSize(horiz, vert);
             for (int i = 0; i < arr.GetLength(0); i++)
             {
@@ -90,44 +68,49 @@ namespace Labirint
                     Console.Write(arr[i, j]);
                 }
             }
+            
         }
+        // <summary>
+        
+       ////горизонтальные дорожки
+       // public static void DrawHL(string[,] arr, int a, int b, int c, int d, int e)
+       // {
+       //     int v = 0;
+       //     int g = 0;
+       //     Console.SetCursorPosition(x + 3, y);
 
-        //горизонтальные дорожки
-        public static void DrawHL(string[,] arr, int a, int b, int c, int d, int e)
-        {
-            int v = 0;
-            int g = 0;
-            Console.SetCursorPosition(x + 3, y);
+       //     for (int i = 0; i < a; i++)
+       //     { Console.Write(" a" + i); } //ставлю буковки чтоб видеть где что сработало
+       //     Console.SetCursorPosition(x + 10, y + 5);
+       //     for (int i = 0; i < b; i++)
+       //     { Console.Write(" b" + i); }
+       //     Console.SetCursorPosition(x + 30, y + 10);
+       //     for (int i = 0; i < c; i++)
+       //     { Console.Write(" c" + i); }
+       //     Console.SetCursorPosition(x + 50, y + 15);
+       //     for (int i = 0; i < d; i++)
+       //     { Console.Write(" d" + i); }
+       //     Console.SetCursorPosition(x + 70, y + 17);
+       //     for (int i = 0; i < e; i++)
+       //     { Console.Write(" e" + i); }
+       // }
 
-            for (int i = 0; i < a; i++)
-            { Console.Write(" a" + i); } //ставлю буковки чтоб видеть где что сработало
-            Console.SetCursorPosition(x + 10, y + 5);
-            for (int i = 0; i < b; i++)
-            { Console.Write(" b" + i); }
-            Console.SetCursorPosition(x + 30, y + 10);
-            for (int i = 0; i < c; i++)
-            { Console.Write(" c" + i); }
-            Console.SetCursorPosition(x + 50, y + 15);
-            for (int i = 0; i < d; i++)
-            { Console.Write(" d" + i); }
-            Console.SetCursorPosition(x + 70, y + 17);
-            for (int i = 0; i < e; i++)
-            { Console.Write(" e" + i); }
-        }
+       // //вертикальные дорожки
+       // public static void DrawVL(string[,] arr)
+       // {
+       //     Console.SetCursorPosition(x + 3, y);
+       //     for (int i = 0; i < 10; i++)
+       //     {
+       //         string g = "g";
+       //         y = 20 + i;
+       //         x = i;
+       //         arr[i, y] = g;
+       //         Console.Write(arr[i, y]);
+       //     }
+       // }
 
-        //вертикальные дорожки
-        public static void DrawVL(string[,] arr)
-        {
-            Console.SetCursorPosition(x + 3, y);
-            for (int i = 0; i < 10; i++)
-            {
-                string g = "g";
-                y = 20+i;
-                x = i;
-                arr[i, y] = g;
-                Console.Write(arr[i, y]);
-            }
-        }
+        // </summary>
+
 
 
 
@@ -147,28 +130,46 @@ namespace Labirint
                     case ConsoleKey.LeftArrow: x--; break;
                     case ConsoleKey.RightArrow: x++; break;
                 }
-                if (x < 0) x = 15;
-                if (y < 0) y = 15;
+                if (x < 0) x = 0;
+                if (y < 0) y = 0;
+                
                 Console.SetCursorPosition(x, y);
-                Console.Write(O);
+                //Console.Write(O);
+                Eat(box);
             }
         }
 
-        /// ///////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-        //////////////////////////////////////////////////////////////////////////////////////
+        public static void Eat(string[,] arr)
+        {
+            
+            if (arr[x,y] == point) {count += 1;}
+            else if (arr[x, y] == wall)
+            {
+                ConsoleKeyInfo tr = Console.ReadKey(false);
+                switch (tr.Key)
+                {
+                    case ConsoleKey.UpArrow: ; break;
+                    case ConsoleKey.DownArrow: ; break;
+                    case ConsoleKey.LeftArrow: ; break;
+                    case ConsoleKey.RightArrow: ; break;
+                }
+            }
+           
+        }
 
         static void Main(string[] args)
         {
+            Console.WriteLine(count);
+            
             GenMap();
             ShowMap(box);
+            Push();
             //DrawHL(box, 10, 10, 10, 10, 10);
             //DrawVL(box);
-            Push();
+            //Eat(box);
+            
+            
+            
             Console.ReadLine();
         }
 
